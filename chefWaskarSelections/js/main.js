492d502614cee9f20a675dc38314a9b5b2ab1527
Waskar Paulino <155832641+traksaw@users.noreply.github.com>
2024-10-18 23:37:36 -0400

    v1.0

---
+++ chefWaskarSelections/js/main.js
@@ -0,0 +1,41 @@
+document.querySelector('#btnInput').addEventListener('click',getMeFood)
+
+function getMeFood(){
+    //this is input field's value
+const foodChoice = document.querySelector('#foodInput').value
+const url = `https://api.api-ninjas.com/v1/recipe?query=${foodChoice}`;
+const options = {
+	method: 'GET',
+	headers: {
+		'X-Api-Key': 'EXyOyn6Z1ROLqILk1NbYYA==3uQH1TRzZMveYzWa',
+		'Content-Type': 'application/json'
+	},
+};
+
+fetch (url, options)
+.then (res => res.json())
+.then(data => {
+    // console.log(data,'data')
+    //creates a randomizer for items in array
+    const randomNum = Math.floor(Math.random() * 10)
+    //variables for each part of the array
+    const recipe = data[randomNum]
+    const ingredients = recipe.ingredients
+    // console.log(ingredients)
+    const instructions = recipe.instructions
+    const servings = recipe.servings
+    const title = recipe.title
+    
+    function nameMyMeal(){
+    document.querySelector('#dishTitle').textContent = `${title}`
+    document.querySelector('#dishIngredients').textContent = `Ingredients -- ${ingredients}`
+    document.querySelector('#dishInstructions').textContent = instructions
+    document.querySelector('#dishServings').textContent = `Servings -- ${servings}`
+    foodChoice.value.textContent = ' '
+    }
+    nameMyMeal()
+})
+.catch(err =>{
+    console.log(`error ${err}`)
+})
+}
